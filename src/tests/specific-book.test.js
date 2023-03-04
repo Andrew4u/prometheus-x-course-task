54d6048be652420b2b6633ad1ab160145d8dabb6
Andrew4u <andruxa.xa@gmail.com>
2023-03-05 00:16:05 +0200

    CSS

--- src/tests/specific-book.test.js
+++ src/tests/specific-book.test.js
@@ -3,7 +3,7 @@ import { render, fireEvent } from "@testing-library/react";
 import SpecificBook from "../components/Specific-book";
 
 describe("SpecificBook", () => {
-  test("increments count on clicking + button", () => {
+  it("increments count on clicking + button", () => {
     const { getByTestId, getByText } = render(<SpecificBook />);
     const incrementButton = getByTestId("plus");
     const countInput = getByText("1");
@@ -13,7 +13,7 @@ describe("SpecificBook", () => {
     expect(countInput).toHaveTextContent("2");
   });
 
-  test("decrements count on clicking - button", () => {
+  it("decrements count on clicking - button", () => {
     const { getByTestId, getByText } = render(<SpecificBook />);
     const decrementButton = getByTestId("minus");
     const countInput = getByText("1");
